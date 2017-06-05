f9486064e20ddf49a096de71db74da410b3cd8eb
ahjummah <jessacabigas@gmail.com>
2017-06-05 16:16:21 +0800

    Updated engine.

---
+++ TFD_/Engine.py
@@ -0,0 +1,109 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+import pickle
+import sys
+from Preprocessing import remove_punctuations
+from Preprocessing import remove_stopwords
+from sklearn.utils import compute_class_weight
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+
+# In[11]:
+
+
+with open('Models/classifier.pkl', 'rb') as f:
+    clf = pickle.load(f)
+    
+with open('Models/vectorizer.pkl', 'rb') as g:
+    vectorizer = pickle.load(g)
+    
+with open('Models/lsa.pkl', 'rb') as h:
+    lsa = pickle.load(h)
+        
+
+
+# In[12]:
+
+
+# a = raw_input("Enter:\n")
+a = "@ice you are so fucking stupid dude!"
+a = remove_punctuations.removePunctuations(a)
+a = remove_stopwords.remove_words(a)
+print a
+X = vectorizer.transform([a]) 
+tfidf = X.toarray()
+lsa_ = lsa.transform(X)
+final_representation = np.concatenate((tfidf,lsa_),axis=1)
+print "output",clf.predict(final_representation)
+
+label = []
+_input = raw_input("Do you feel like you are being bashed?[0/1]:\n")
+label.append(_input)
+
+
+# In[5]:
+
+
+b = "you are just beautiful"
+b = remove_punctuations.removePunctuations(b)
+b = remove_stopwords.remove_words(b)
+
+X1 = vectorizer.transform([b])
+tfidf = X1.toarray()
+lsa_ = lsa.transform(X1)
+final_representation = np.concatenate((tfidf,lsa_),axis=1)
+print "output",clf.predict(final_representation)
+_input = raw_input("Do you feel like you are being bashed?[0/1]:\n")
+label.append(_input)
+
+
+# In[13]:
+
+
+new_feed = []
+new_feed.append(a)
+# new_feed.append(b)
+c = []
+
+for tweet in new_feed:
+    c.append(remove_stopwords.remove_words(remove_punctuations.removePunctuations(tweet)))
+    
+X_new = np.array(c)   
+y_new = np.array(label)
+le = LabelEncoder()
+y_encoded = le.fit_transform(y_new)
+print X_new.shape
+print y_new.shape
+
+
+# In[14]:
+
+
+X2 = vectorizer.transform(X_new)
+tfidf = X2.toarray()
+lsa_ = lsa.transform(X2)
+final_representation = np.concatenate((tfidf,lsa_),axis=1)
+
+
+# In[15]:
+
+
+# class_weights = compute_class_weight('balanced', [0, 1], y_new)
+# class_weight_dictionary = {1:class_weights[0], 1:class_weights[1]}
+clf.partial_fit(final_representation,y_encoded)
+
+
+# In[16]:
+
+
+# save to pickle
+import pickle
+
+with open('Models/classifier.pkl', 'wb') as f:
+    pickle.dump(clf, f)
+
--- TFD_/README.md
+++ TFD_/README.md
@@ -1,1 +1,2 @@
 # TFD_
+#Run jupyter nbconvert --to script .\Engine.ipynb to convert ipython to python script
